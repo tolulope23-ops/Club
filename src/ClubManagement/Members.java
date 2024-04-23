856390934ef1ba6095ae795c7c964c21e87b4d3a
Adeyemi Racheal <adeyemitoluadedoyin@gmail.com>
2024-04-23 15:17:34 +0100

    Today

--- src/ClubManagement/Members.java
+++ src/ClubManagement/Members.java
@@ -5,4 +5,43 @@ public class Members {
     private char memberType;
     private String name;
     private Double Fees;
+
+    public Members(int ID, char memberType, String name, Double Fees){}
+
+    public int getID() {
+        return ID;
+    }
+
+    public void setID(int id) {
+        ID = id;
+    }
+
+    public char getMemberType() {
+        return memberType;
+    }
+
+    public void setMemberType(char memberType) {
+        this.memberType = memberType;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Double getFees() {
+        return Fees;
+    }
+
+    public void setFees(Double fees) {
+        Fees = fees;
+    }
+
+    @Override
+    public String toString(){
+        return "The member is:" + ID + name + memberType;
+    }
 }
